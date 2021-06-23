175a1a84e20058d4b07e6f5b8c287fab01bf0de1
fuad <fuad221@outlook.com>
2021-06-23 09:23:09 +0200

    still working

--- src/componenets/Nav.js
+++ src/componenets/Nav.js
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
+// import { Link } from 'gatsby'
+// import { StaticImage } from 'gatsby-plugin-image'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 
 
@@ -10,57 +12,25 @@ const Nav = () => {
   return (
 
 
-
-
     <nav className="navbar navbar-expand-lg bg-primary">
 
-      {/* <StaticImage
-              alt="Logo mindChase"
-              src="../images/logo.png"
-            /> */}
 
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li>
-            <Link to="/" className="text-white text-decoration-none " >
-              Home
-            </Link>
-        </li>
+      <Navbar bg="primary" variant="dark">
+          <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">about</Nav.Link>
+            <Nav.Link href="#workshops">workshops</Nav.Link>
+            <Nav.Link href="#digitalcampus">digitalcampus</Nav.Link>
+            <Nav.Link href="#news">news</Nav.Link>
+            <Nav.Link href="#games">games</Nav.Link>
+            <Nav.Link href="#contact">contact</Nav.Link>
 
-        <li>
-            <Link to="/about" className="text-white text-decoration-none">
-              About
-            </Link>
-        </li>
-
-        <li>
-            <Link to="/workshops" className="text-white text-decoration-none">
-            Workshops
-            </Link>
-          </li>
-        
-          <li>
-            <Link to="/digitalcampus" className="text-white text-decoration-none ">
-            Digital campus
-            </Link>
-          </li>
-
-          {/* <li>
-            <Link to="/news" className="text-white text-decoration-none">
-            News
-            </Link>
-          </li>
-     */}
-
-          <li>
-            <Link to="/contact" className="text-white text-decoration-none">
-            Contact
-            </Link>
-          </li>
-
-        
-
-      </ul>
-
+          </Nav>
+          </Container>
+        </Navbar>
+    
     </nav>
   );
 };
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,70 +1,70 @@
-// import * as React from "react";
-// import { useStaticQuery, graphql } from "gatsby";
-// import { Link } from 'gatsby'
-// import Nav from "../componenets/Nav";
-// import { StaticImage } from 'gatsby-plugin-image'
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Link } from 'gatsby'
+import Nav from "../componenets/Nav";
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 
-// const News = () => {
-//   // graphQL api call
-//   const data = useStaticQuery(
-//     graphql`
-//       query {
-//         allGhostPost {
-//           edges {
-//             node {
-//               id
-//               title
-//               html
-//               published_at
-//               tags {
-//                 id
-//                 name
-//                 slug
-//               }
-//               authors {
-//                 id
-//                 name
-//                 profile_image
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-//   );
+const News = () => {
+  // graphQL api call
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allGhostPost {
+          edges {
+            node {
+              id
+              title
+              html
+              published_at
+              tags {
+                id
+                name
+                slug
+              }
+              authors {
+                id
+                name
+                profile_image
+              }
+            }
+          }
+        }
+      }
+    `
+  );
 
-//   // console.log(data.allGhostPost.edges);
+  // console.log(data.allGhostPost.edges);
 
-//   return (
-//     // <>
-//     //   <Nav />
-//     //   {data
-//     //     ? data.allGhostPost.edges.map(({ node }) => {
-//     //       return (
+  return (
+    <>
+       <Nav />
+       {data
+        ? data.allGhostPost.edges.map(({ node }) => {
+          return (
 
-//     //         <div className="card" style="width: 18rem; key={node.id}">
+            <div className="card" style="width: 18rem; key={node.id}">
 
-//     //         {/* <StaticImage
-//     //           alt="Logo mindChase"
-//     //           src="./img/logo.png"
-//     //         /> */}
+            {/* <StaticImage
+              alt="Logo mindChase"
+              src="./img/logo.png"
+            /> */}
 
-//     //           <div className="card-body">
-//     //             <h5 className="card-title">{node.title}</h5>
-//     //             <article className="card-text" dangerouslySetInnerHTML={{ __html: node.html }} /> 
-//     //             <Link to="/" className="btn btn-primary">Go to article</Link>
-//     //           </div>
-//     //         </div>
-//     //       )
+              <div className="card-body">
+                <h5 className="card-title">{node.title}</h5>
+                <article className="card-text" dangerouslySetInnerHTML={{ __html: node.html }} /> 
+                <Link to="/" className="btn btn-primary">Go to article</Link>
+              </div>
+            </div>
+          )
 
-//     //     })
+        })
 
-//     //     : null}
+        : null}
 
-//     // </>
-//   );
-// }
+    </>
+  );
+}
 
-// export default News;
+export default News;
