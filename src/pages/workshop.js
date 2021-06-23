f7705fdc4f03a7ec49f9613d552c13b313740b1e
fuad <fuad221@outlook.com>
2021-06-23 11:16:08 +0200

    i cread the cards for the workshpe

---
+++ src/pages/workshop.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+
+function workshop() {
+    return (
+        <div>
+
+            <Card style={{ width: '18rem' }}>
+                <StaticImage
+                    alt="Logo mindChase"
+                    src="../images/logo.png"
+                />
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Empowerment</Card.Title>
+                    <Card.Text>
+                        *************Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+
+            <Card style={{ width: '18rem' }}>
+                <StaticImage
+                    alt="Logo mindChase"
+                    src="../images/logo.png"
+                />
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Motivation</Card.Title>
+                    <Card.Text>
+                        *************Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+
+            <Card style={{ width: '18rem' }}>
+                <StaticImage
+                    alt="Logo mindChase"
+                    src="../images/logo.png"
+                />
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Presentation</Card.Title>
+                    <Card.Text>
+                        *************Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+
+            <Card style={{ width: '18rem' }}>
+                <StaticImage
+                    alt="Logo mindChase"
+                    src="../images/logo.png"
+                />
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>self Confidence</Card.Title>
+                    <Card.Text>
+                        *************Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+
+            <Card style={{ width: '18rem' }}>
+                <StaticImage
+                    alt="Logo mindChase"
+                    src="../images/logo.png"
+                />
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Stress Management</Card.Title>
+                    <Card.Text>
+                        *************Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+
+            <Card style={{ width: '18rem' }}>
+                <StaticImage
+                    alt="Logo mindChase"
+                    src="../images/logo.png"
+                />
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Time Management</Card.Title>
+                    <Card.Text>
+                        *************Some quick example text to build on the card title and make up the bulk of
+                        the card's content. 
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+        </div>
+    )
+}
+
+export default workshop
