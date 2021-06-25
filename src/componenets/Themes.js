f34af7125e6e33ae20354c8ef5ceb49b0d8bd7a4
Doven <doven.obertinca@gmail.com>
2021-06-25 15:17:47 +0200

    social media with black banner

--- src/componenets/Themes.js
+++ src/componenets/Themes.js
@@ -1,16 +1,4 @@
 import * as React from "react";
-import {lightTheme, darkTheme, GlobalStyles } from '../componenets/Themes.js'
 
 
-export const lightTheme = {
-    body: "#fff",
-    fontColor:"#000"
 
-}
-
-export const darkTheme = {
-    body:"#000",
-    fontColor:"#fff",
-}
-
-export const GlobalStyles = createlobalStyles;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,19 @@ import Header from "../componenets/Header";
 import Body from "../componenets/Body";
 import Banner from "../componenets/Banner";
 import PopUp from "../componenets/Modal";
-import { useState } from "react"
-import styled, {ThemeProvider} from "styled-components";
-import {lightTheme, darkTheme, GlobalStyles } from '../componenets/Themes.js'
-
 
 const IndexPage = () => {
 
-  const [theme, setTheme] = useState("light")
-
-  const themeToggler = () =>{
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  }
   return (
-  
     <>
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Nav />
-        <PopUp />
-        <Header />
-        <Body />
-        <Banner />
-        <Footer/>
-  
-      </ThemeProvider>
+      <Nav />
+      <PopUp />
+      <Header />
+      <Body />
+      <Banner />
+      <Footer />
     </>
   );
-    
 };
- 
+
 export default IndexPage;
