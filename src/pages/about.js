4096e2489ae8bb229ad2a959746fcc4f75427041
Doven <doven.obertinca@gmail.com>
2021-07-21 15:14:46 +0200

    about with fade overlay and box shadows

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,105 +3,118 @@ import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/Layout";
 import Seo from "../components/seo";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+//import h5 class={ card-title } from "react-bootstrap/Card";
+import {
+  hover,
+  hover1,
+  hover2,
+  hover3,
+  hover4,
+  overlay,
+  hoverMe,
+  text,
+  container,
+} from "./about.module.scss";
 
-// markup
 const AboutPage = () => {
   return (
     <Layout>
       <Seo title="About" />
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <Card style={{ width: "18rem" }}>
-              <div className="col">
-                <StaticImage
-                  alt="Marco, the only man on a team"
-                  src="../images/team/Marco/marco.jpg"
-                  layout="fixed"
-                  width={300}
-                  height={300}
-                />
+      <div className={container}>
+        {/* <div className={hoverMe}> */}
+          <div className="row">
+            <div className="col-sm-4">
+              <div className={hover}>
+                <div className="card" style={{ width: "26rem" }}>
+                  <StaticImage
+                    alt="Marco, the only man on a team"
+                    src="../images/team/Marco/marco.jpg"
+                    layout="fixed"
+                    width={410}
+                    height={300}
+                  />
+
+                  <div className="card-body">
+                    <div class={overlay}>
+                      <h5 class={text}>Marco</h5>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <Card.Body>
-                <Card.Title>Marco</Card.Title>
-                <Card.Text>Marco, the only man on a team</Card.Text>
-                <Button variant="primary">More about Marco...</Button>
-              </Card.Body>
-            </Card>
+            </div>
+          {/* </div> */}
+          {/* <div className={hoverMe}> */}
+            <div className="col-sm-4">
+              <div className={hover1}>
+                <div className="card" style={{ width: "26rem" }}>
+                  <StaticImage
+                    alt="Karo, CEO of mindchase"
+                    src="../images/team/Karo/karo.JPG"
+                    layout="fixed"
+                    width={410}
+                    height={400}
+                  />
+
+                  <div className="card-body">
+                    <div class={overlay}>
+                      <h5 class={text}>Karo</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          {/* </div> */}
+          <div className="col-sm-4">
+            <div className={hover2}>
+              <div className="card" style={{ width: "26rem" }}>
+                <StaticImage
+                  alt="Phyllis"
+                  src="../images/team/Phyllis/phyllis.JPG"
+                  layout="fixed"
+                  width={410}
+                  height={400}
+                />
+
+                <div className="card-body">
+                  <h5 class="card-title">Phyllis</h5>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col">
-            <Card style={{ width: "18rem" }}>
-              <StaticImage
-                alt="Karo, CEO of mindchase"
-                src="../images/team/Karo/karo.JPG"
-                layout="fixed"
-                width={300}
-                height={300}
-              />
+          <div className="row">
+            <div className="col-sm-4">
+              <div className={hover3}>
+                <div className="card" style={{ width: "26rem" }}>
+                  <StaticImage
+                    alt="Kathleen"
+                    src="../images/team/Kathleen/kathleen.JPEG"
+                    layout="fixed"
+                    width={400}
+                    height={300}
+                  />
+                  <div className="card-body">
+                    <h5 class="card-title">Kathleen</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className={hover4}>
+                <div className="card" style={{ width: "26rem" }}>
+                  <StaticImage
+                    alt="Franzi, second CEO of mindchase"
+                    src="../images/team/Franzi/franzi.jpg"
+                    layout="fixed"
+                    width={400}
+                    height={300}
+                  />
 
-              <Card.Body>
-                <Card.Title>Karo</Card.Title>
-                <Card.Text>Karo, CEO of mindchase</Card.Text>
-                <Button variant="primary">More about Karo...</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col">
-            <Card style={{ width: "18rem" }}>
-              <StaticImage
-                alt="Phyllis"
-                src="../images/team/Phyllis/phyllis.JPG"
-                layout="fixed"
-                width={300}
-                height={400}
-              />
-
-              <Card.Body>
-                <Card.Title>Phyllis</Card.Title>
-                <Card.Text>Phyllis</Card.Text>
-                <Button variant="primary">More about Phyllis...</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <Card style={{ width: "18rem" }}>
-              <StaticImage
-                alt="Kathleen"
-                src="../images/team/Kathleen/kathleen.JPEG"
-                layout="fixed"
-                width={300}
-                height={300}
-              />
-
-              <Card.Body>
-                <Card.Title>Kathleen</Card.Title>
-                <Card.Text>Kathleen</Card.Text>
-                <Button variant="primary">More about Kathleen...</Button>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col">
-            <Card style={{ width: "18rem" }}>
-              <StaticImage
-                alt="Franzi, second CEO of mindchase"
-                src="../images/team/Franzi/franzi.jpg"
-                layout="fixed"
-                width={300}
-                height={300}
-              />
-
-              <Card.Body>
-                <Card.Title>Franzi</Card.Title>
-                <Card.Text>Franzi, second CEO of mindchase</Card.Text>
-                <Button variant="primary">
-                    More about Franzi...
-                </Button>
-              </Card.Body>
-            </Card>
+                  <div className="card-body">
+                    <h5 class="card-title">Franzi</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
