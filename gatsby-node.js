76a8596bc32ced2014b7d210d6ee390d2872fdb1
Paul <you@example.com>
2021-07-26 16:47:52 +0200

    its aaalllllliiiiiiiiiivvvvvvvvvvveeeeeeeeeeeeeeeeeeee

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,51 +1,51 @@
-// // note posts here refers to news items
-// const path = require("path");
+// note posts here refers to news items
+const path = require("path");
 
-// // add the html here so it can be passed thru context
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions;
-//   const result = await graphql(`
-//     {
-//       allGhostPost {
-//         edges {
-//           node {
-//             title
-//             slug
-//             html
-//             feature_image
-//           }
-//         }
-//       }
-//     }
-//   `);
+// add the html here so it can be passed thru context
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allGhostPost {
+        edges {
+          node {
+            title
+            slug
+            html
+            feature_image
+          }
+        }
+      }
+    }
+  `);
 
-//   // Check for any errors
-//   if (result.errors) {
-//     throw new Error(result.errors);
-//   }
+  // Check for any errors
+  if (result.errors) {
+    throw new Error(result.errors);
+  }
 
-//   // Extract query results
-//   const posts = result.data.allGhostPost.edges;
+  // Extract query results
+  const posts = result.data.allGhostPost.edges;
   
-//   // Load templates
-//   const postTemplate = path.resolve(`./src/templates/NewsItem.js`);
+  // Load templates
+  const postTemplate = path.resolve(`./src/templates/NewsItem.js`);
 
-//   // Create post pages
-//   posts.forEach(({ node }) => {
-//     // This part here defines, that our posts will use
-//     // a `/:slug/` permalink.
-//     node.url = `/news/${node.slug}/`;
-//     // console.log("file node.js", node.url);
-//     createPage({
-//       path: node.url,
-//       component: postTemplate,
-//       context: {
-//         // Data passed to context is available
-//         // in page queries as GraphQL variables.
-//         slug: node.slug,
-//         html: node.html,
-//         feature_image: node.feature_image
-//       },
-//     });
-//   });
-// };
+  // Create post pages
+  posts.forEach(({ node }) => {
+    // This part here defines, that our posts will use
+    // a `/:slug/` permalink.
+    node.url = `/news/${node.slug}/`;
+    // console.log("file node.js", node.url);
+    createPage({
+      path: node.url,
+      component: postTemplate,
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.slug,
+        html: node.html,
+        feature_image: node.feature_image
+      },
+    });
+  });
+};
--- src/components/newsBody/NewsHeader.js
+++ src/components/newsBody/NewsHeader.js
@@ -1,71 +1,71 @@
-// import React from "react";
-// import { useStaticQuery, graphql, Link } from "gatsby";
-// // add as module if I can't get dynamic image working
-// // import Img from "gatsby-image";
-// import Button from "react-bootstrap/Button";
-// import Col from "react-bootstrap/Col";
-// import Row from "react-bootstrap/Row";
-// import Image from "react-bootstrap/Image";
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
+// add as module if I can't get dynamic image working
+// import Img from "gatsby-image";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
 
-// const NewsHeader = () => {
-//   const data = useStaticQuery(
-//     graphql`
-//       query {
-//         allGhostPost {
-//           edges {
-//             node {
-//               id
-//               title
-//               feature_image
-//               html
-//               published_at(formatString: "MMMM D, YYYY")
-//               excerpt
-//               tags {
-//                 id
-//                 name
-//                 slug
-//               }
-//               authors {
-//                 id
-//                 name
-//                 profile_image
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-//   );
+const NewsHeader = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allGhostPost {
+          edges {
+            node {
+              id
+              title
+              feature_image
+              html
+              published_at(formatString: "MMMM D, YYYY")
+              excerpt
+              tags {
+                id
+                name
+                slug
+              }
+              authors {
+                id
+                name
+                profile_image
+              }
+            }
+          }
+        }
+      }
+    `
+  );
 
-//   return (
-//     // map so this randomizes on refresh and
-//     // filter only for feature_image
+  return (
+    // map so this randomizes on refresh and
+    // filter only for feature_image
 
-//     <Row fluid className="py-5">
-//       <Col xs={12} md={6}>
-//         <Image
-//           src={data.allGhostPost.edges[0].node.feature_image}
-//           fluid
-//           alt=""
-//         />
-//       </Col>
-//       <Col xs={12} md={6}>
-//         <Button variant="outline-dark" size="sm">
-//           {data.allGhostPost.edges[1].node.tags[0].name}
-//         </Button>
-//         <h1 className="display-5 fw-bold">
-//           {data.allGhostPost.edges[0].node.title}
-//         </h1>
-//         <p>
-//           by {data.allGhostPost.edges[0].node.authors[0].name} on{" "}
-//           {data.allGhostPost.edges[0].node.published_at}
-//         </p>
-//         <Button variant="danger" className="text-uppercase">
-//           read more
-//         </Button>
-//       </Col>
-//     </Row>
-//   );
-// };
+    <Row fluid className="py-5">
+      <Col xs={12} md={6}>
+        <Image
+          src={data.allGhostPost.edges[0].node.feature_image}
+          fluid
+          alt=""
+        />
+      </Col>
+      <Col xs={12} md={6}>
+        <Button variant="outline-dark" size="sm">
+          {data.allGhostPost.edges[1].node.tags[0].name}
+        </Button>
+        <h1 className="display-5 fw-bold">
+          {data.allGhostPost.edges[0].node.title}
+        </h1>
+        <p>
+          by {data.allGhostPost.edges[0].node.authors[0].name} on{" "}
+          {data.allGhostPost.edges[0].node.published_at}
+        </p>
+        <Button variant="danger" className="text-uppercase">
+          read more
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
-// export default NewsHeader;
+export default NewsHeader;
