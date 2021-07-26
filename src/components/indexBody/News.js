e5eb606f6a13cbac0a330d9caa5936fee1919984
Paul <you@example.com>
2021-07-26 10:26:10 +0200

    working on prod

--- src/components/indexBody/News.js
+++ src/components/indexBody/News.js
@@ -1,60 +1,60 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import { useStaticQuery, graphql, Link } from "gatsby";
-import "./News.module.scss";
-// import { StaticImage, GatsbyImage } from "gatsby-plugin-image";
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import Button from "react-bootstrap/Button";
+// import { useStaticQuery, graphql, Link } from "gatsby";
+// import "./News.module.scss";
+// // import { StaticImage, GatsbyImage } from "gatsby-plugin-image";
 
-const News = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        allGhostPost(limit: 3, sort: { order: DESC, fields: [published_at] }) {
-          edges {
-            node {
-              id
-              title
-              feature_image
-            }
-          }
-        }
-      }
-    `
-  );
+// const News = () => {
+//   const data = useStaticQuery(
+//     graphql`
+//       query {
+//         allGhostPost(limit: 3, sort: { order: DESC, fields: [published_at] }) {
+//           edges {
+//             node {
+//               id
+//               title
+//               feature_image
+//             }
+//           }
+//         }
+//       }
+//     `
+//   );
 
-  // note to self - use filter in GQL query for latest 3 posts - sort news section first
-  return (
-    <>
-      <h2>
-        <u>Stay Updated</u>
-      </h2>
-      <Container>
-        <Row>
-          {data &&
-            data.allGhostPost.edges.map(({ node }) => {
-              return (
-                <Col>
-                  <Link to="/news">
-                    <Card key={node.id} style={{ width: "18rem" }}>
-                      <Card.Img variant="top" src={node.feature_image} />
-                      <Card.Body>
-                        <Card.Title>{node.title}</Card.Title>
-                        <Button variant="danger" className="text-uppercase">
-                          read more
-                        </Button>
-                      </Card.Body>
-                    </Card>
-                  </Link>
-                </Col>
-              );
-            })}
-        </Row>
-      </Container>
-    </>
-  );
-};
+//   // note to self - use filter in GQL query for latest 3 posts - sort news section first
+//   return (
+//     <>
+//       <h2>
+//         <u>Stay Updated</u>
+//       </h2>
+//       <Container>
+//         <Row>
+//           {data &&
+//             data.allGhostPost.edges.map(({ node }) => {
+//               return (
+//                 <Col>
+//                   <Link to="/news">
+//                     <Card key={node.id} style={{ width: "18rem" }}>
+//                       <Card.Img variant="top" src={node.feature_image} />
+//                       <Card.Body>
+//                         <Card.Title>{node.title}</Card.Title>
+//                         <Button variant="danger" className="text-uppercase">
+//                           read more
+//                         </Button>
+//                       </Card.Body>
+//                     </Card>
+//                   </Link>
+//                 </Col>
+//               );
+//             })}
+//         </Row>
+//       </Container>
+//     </>
+//   );
+// };
 
-export default News;
+// export default News;
--- src/templates/NewsItem.js
+++ src/templates/NewsItem.js
@@ -1,42 +1,42 @@
-import React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
-import Layout from "../components/Layout";
-import Seo from "../components/seo";
-import Container from "react-bootstrap/Container";
-import Image from 'react-bootstrap/Image'
+// import React from "react";
+// import { useStaticQuery, graphql, Link } from "gatsby";
+// import { StaticImage } from "gatsby-plugin-image";
+// import Layout from "../components/Layout";
+// import Seo from "../components/seo";
+// import Container from "react-bootstrap/Container";
+// import Image from 'react-bootstrap/Image'
 
-const NewsItem = (context) => {
-  // pull in title, img + img metadata, then full html
-  const NewsItemData = useStaticQuery(
-    graphql`
-      query NewsItem($slug: String) {
-        ghostPost(slug: { eq: $slug }) {
-          ghostId
-          title
-          slug
-          html
-          feature_image
-        }
-      }
-    `
-  );
+// const NewsItem = (context) => {
+//   // pull in title, img + img metadata, then full html
+//   const NewsItemData = useStaticQuery(
+//     graphql`
+//       query NewsItem($slug: String) {
+//         ghostPost(slug: { eq: $slug }) {
+//           ghostId
+//           title
+//           slug
+//           html
+//           feature_image
+//         }
+//       }
+//     `
+//   );
 
-  // access context via pageContext
+//   // access context via pageContext
   
-  return (
-    <Layout>
-      <Seo title={context.pageContext.slug} />
-      <Container fluid>
-        <h1>{context.pageContext.slug}</h1>
-        <p>by -Link- author -Link- on date?</p>
-        <Image src={context.pageContext.feature_image} />
-        <div
-          dangerouslySetInnerHTML={{ __html: context.pageContext.html }}
-        />
-      </Container>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <Seo title={context.pageContext.slug} />
+//       <Container fluid>
+//         <h1>{context.pageContext.slug}</h1>
+//         <p>by -Link- author -Link- on date?</p>
+//         <Image src={context.pageContext.feature_image} />
+//         <div
+//           dangerouslySetInnerHTML={{ __html: context.pageContext.html }}
+//         />
+//       </Container>
+//     </Layout>
+//   );
+// };
 
-export default NewsItem;
+// export default NewsItem;
