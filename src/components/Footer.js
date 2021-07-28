9a35eec8d12e5b8dcdec8f7274664a72d4c68919
Agata Bernat <agata.bernat.art@gmail.com>
2021-07-29 00:44:29 +0200

    latest changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { socialicons, icon, backgroundImage, text } from "./Footer.module.scss";
+import { socialicons, icon, backgroundImage, text, footerLogo } from "./Footer.module.scss";
 import "./Footer.module.scss";
 import { StaticImage } from "gatsby-plugin-image";
 import { Link } from "gatsby";
@@ -11,18 +11,17 @@ const Footer = () => {
     <div className="container-fluid">
       <div className={backgroundImage}>
         <div className="row">
-          <div className="col-12 col-lg-2" layout="fixed justify-content-center text-center">
+          <div className="col-12 col-lg-2">
             <Link className="footer-logo" to="/">
               <StaticImage
                 src="../images/logos/mindchase_logo_komplett_transparent.png"
                 alt="Mindchase logo. Red text, background transparent."
-                layout="fixed"
-                width={250}
-                height={250}
+                className={footerLogo}
+                
               />
             </Link>
           </div>
-          <div className="col-12 col-lg-8" layout="fixed justify-content-center text-center">
+          <div className="col-12 col-lg-7">
             <Form className="text-align:center d-flex justify-content-center">
               <Form.Group className="mt-5 mb-5" controlId="formBasicEmail">
                 <h3 className="mb-2 text-center text-#FF1616 mb-3">
@@ -36,17 +35,10 @@ const Footer = () => {
                 >
                   submit
                 </Link>
-                {/* <Form.Text className="text-white">
-                    This is where I want "email must be formatted correctly" to
-                    render conditionally
-                  </Form.Text> */}
-              </Form.Group>
+                </Form.Group>
             </Form>
           </div>
-          <div
-            className="col-12 col-lg-2"
-            layout="fixed justify-content-center text-center"
-          >
+          <div className="col-12 col-lg-3">
             <>
               <div className={socialicons}>
                 <p>Follow us:</p>
@@ -69,11 +61,11 @@ const Footer = () => {
           </div>
         </div>
         <div className="row">
-          <div className="col-12 col-lg-6" layout="d-flex justify-content-center">
+          <div className="col-12 col-lg-6">
             <p className={text}>
               &copy;{" "}
               {new Date().getFullYear().toString()} mindchase. All Rights
-              Reserved. <br></br> Site developed by Team Vikings  
+              Reserved. <br></br> Made with <span className="heart">♥</span> by Team Vikings.
             </p>
           </div>
         </div>
